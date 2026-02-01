3ccab87e9021cf30fbb1845038066698353d37f6
Sakib <sjsakib.bd@gmail.com>
2026-02-01 20:39:48 +0600

    added new events

--- fb-script.js
+++ fb-script.js
@@ -18,7 +18,7 @@
  */
 
 async function wait(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const existingLinks = [
@@ -79,10 +79,81 @@ const existingLinks = [
   "https://www.facebook.com/events/4114741255449245/",
 ];
 
+const existiingLinkts2 = ['https://www.facebook.com/events/1050822980598826/',
+  'https://www.facebook.com/events/1070178581747490/',
+  'https://www.facebook.com/events/1075528950815369/',
+  'https://www.facebook.com/events/1079052274408765/',
+  'https://www.facebook.com/events/1086855456646781/',
+  'https://www.facebook.com/events/1112768410830667/',
+  'https://www.facebook.com/events/1118960576659043/',
+  'https://www.facebook.com/events/1124892606242993/',
+  'https://www.facebook.com/events/1167593055015434/',
+  'https://www.facebook.com/events/1200537458509104/',
+  'https://www.facebook.com/events/1202979818322561/',
+  'https://www.facebook.com/events/1275849290988196/',
+  'https://www.facebook.com/events/1280320550366447/',
+  'https://www.facebook.com/events/1282929109365681/',
+  'https://www.facebook.com/events/1310925940683664/',
+  'https://www.facebook.com/events/1325024898637276/',
+  'https://www.facebook.com/events/1327272625627369/',
+  'https://www.facebook.com/events/1386860609002490/',
+  'https://www.facebook.com/events/1467028837838378/',
+  'https://www.facebook.com/events/1508440780167582/',
+  'https://www.facebook.com/events/1586485422746451/',
+  'https://www.facebook.com/events/1594926954814145/',
+  'https://www.facebook.com/events/1688631351829487/',
+  'https://www.facebook.com/events/2006972836779088/',
+  'https://www.facebook.com/events/2193032994458000/',
+  'https://www.facebook.com/events/2272538536472882/',
+  'https://www.facebook.com/events/2618506141831658/',
+  'https://www.facebook.com/events/312098505035861/',
+  'https://www.facebook.com/events/3969084926735782/',
+  'https://www.facebook.com/events/490327947462459/',
+  'https://www.facebook.com/events/555332594315963/',
+  'https://www.facebook.com/events/575514851888090/',
+  'https://www.facebook.com/events/578069121638871/',
+  'https://www.facebook.com/events/616863397424645/',
+  'https://www.facebook.com/events/627843053219631/',
+  'https://www.facebook.com/events/696249480066810/',
+  'https://www.facebook.com/events/718415788025173/',
+  'https://www.facebook.com/events/719691141122381/',
+  'https://www.facebook.com/events/754073907683174/',
+  'https://www.facebook.com/events/788897559329643/',
+  'https://www.facebook.com/events/919710343656806/',
+  'https://www.facebook.com/events/922324983164455/',
+  'https://www.facebook.com/events/969843514710713/',
+  'https://www.facebook.com/events/990306433290594/',
+  'https://www.facebook.com/events/1874907930090863/',
+  'https://www.facebook.com/events/2197453854065800/',
+  'https://www.facebook.com/events/1452407899154459/',
+  'https://www.facebook.com/events/853133160310612/',
+  'https://www.facebook.com/events/1272401211011481/',
+  'https://www.facebook.com/events/813895684918436/',
+  'https://www.facebook.com/events/789231534016799/',
+  'https://www.facebook.com/events/1180304947491265/',
+  'https://www.facebook.com/events/1889865724934564/',
+  'https://www.facebook.com/events/913844177217876/',
+  'https://www.facebook.com/events/4114741255449245/',
+  'https://www.facebook.com/events/1235906178386844/',
+  'https://www.facebook.com/events/1499498291346491/',
+  'https://www.facebook.com/events/721480814119087/',
+  'https://www.facebook.com/events/843141418464127/',
+  'https://www.facebook.com/events/2030356421080273/',
+  'https://www.facebook.com/events/1101355147958368/',
+  'https://www.facebook.com/events/1466087721119205/',
+  'https://www.facebook.com/events/1259408212784091/',
+  'https://www.facebook.com/events/4270751369837883/',
+  'https://www.facebook.com/events/647117035126903/',
+  'https://www.facebook.com/events/1380842847079499/',
+  'https://www.facebook.com/events/1390704139352040/',
+  'https://www.facebook.com/events/2662673687426287/',
+  'https://www.facebook.com/events/1082351283873934/',
+  'https://www.facebook.com/events/736122422490820/',]
+
 (async function () {
   console.log(
-    "%cStarting the Event Link Scraper...",
-    "color: #28a745; font-weight: bold;",
+    '%cStarting the Event Link Scraper...',
+    'color: #28a745; font-weight: bold;',
   );
 
   // Use a Set to store unique links automatically.
@@ -94,14 +165,14 @@ const existingLinks = [
   while (true) {
     const seeMoreButtons = Array.from(
       document.querySelectorAll('div[role="button"]'),
-    ).filter((btn) => btn.innerText.toLowerCase().includes("see more"));
+    ).filter(btn => btn.innerText.toLowerCase().includes('see more'));
 
     if (seeMoreButtons.length >= 2) {
       console.log(
         `%cClicking "See More" button to load more content..., Click count: ${
           seeMoreClickCount
         }`,
-        "color: #ffc107;",
+        'color: #ffc107;',
       );
       seeMoreButtons[0].click();
       seeMoreClickCount++;
@@ -125,21 +196,21 @@ const existingLinks = [
     // Find all anchor tags where the href attribute contains "/events/".
     const eventLinks = document.querySelectorAll('a[href*="/events/"]');
 
-    eventLinks.forEach((link) => {
+    eventLinks.forEach(link => {
       // The .href property gives the full, absolute URL.
       const fullUrl = link.href;
       let cleanedUrl;
 
       // Clean the URL by removing the query string and everything after it.
-      if (fullUrl.includes("?")) {
-        cleanedUrl = fullUrl.substring(0, fullUrl.indexOf("?"));
+      if (fullUrl.includes('?')) {
+        cleanedUrl = fullUrl.substring(0, fullUrl.indexOf('?'));
       } else {
         cleanedUrl = fullUrl;
       }
 
       // If the link isn't already in our set, log it and add it.
       if (!uniqueCleanedLinks.has(cleanedUrl)) {
-        console.log(`%c  -> New link found: ${cleanedUrl}`, "color: #007bff;");
+        console.log(`%c  -> New link found: ${cleanedUrl}`, 'color: #007bff;');
         uniqueCleanedLinks.add(cleanedUrl);
       }
     });
@@ -149,7 +220,7 @@ const existingLinks = [
       const newLinksFound = uniqueCleanedLinks.size - currentLinkCount;
       console.log(
         `%cFound ${newLinksFound} new link(s) this interval. Total unique links: ${uniqueCleanedLinks.size}`,
-        "color: #17a2b8;",
+        'color: #17a2b8;',
       );
       noNewLinksCounter = 0;
       // Reset the idle counter since we found new content.
@@ -165,7 +236,7 @@ const existingLinks = [
       clearInterval(scraperIntervalId);
       console.log(
         `\n%cScraper stopped automatically after ${STOP_AFTER_IDLE_INTERVALS} scrolls with no new links.`,
-        "color: #dc3545; font-weight: bold;",
+        'color: #dc3545; font-weight: bold;',
       );
       logFinalResults();
     } else {
@@ -175,11 +246,11 @@ const existingLinks = [
   };
   // This function logs the final results to the console.
   const logFinalResults = () => {
-    console.log("\n--- Scraping Complete ---");
+    console.log('\n--- Scraping Complete ---');
     console.log(`Total unique event links found: ${uniqueCleanedLinks.size}`);
     console.log(
-      "%cTo copy all links to your clipboard, run the following command in the console:",
-      "font-weight: bold;",
+      '%cTo copy all links to your clipboard, run the following command in the console:',
+      'font-weight: bold;',
     );
 
     // Make the data easily accessible in the console.
@@ -189,10 +260,10 @@ const existingLinks = [
   // Start the scraper.
   const scraperIntervalId = setInterval(scrapeAndScroll, SCROLL_INTERVAL);
 
-  console.log("Scraper is running. It will scroll down every 2 seconds.");
+  console.log('Scraper is running. It will scroll down every 2 seconds.');
   console.log(
-    "%cTo stop manually, type this into the console and press Enter:",
-    "color: #ffc107;",
+    '%cTo stop manually, type this into the console and press Enter:',
+    'color: #ffc107;',
   );
   console.log(`clearInterval(${scraperIntervalId})`);
 })();
